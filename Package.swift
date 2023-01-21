37b58e291386af2030a7e85d8e791a8c165b4561
André Rohrbeck <andre.rohrbeck@tuvsud.com>
2023-01-22 00:17:53 +0100

    Changed SwiftLint revision.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint", revision: "8d4ebcc723fb5b8c13bd67227b640f5675943309")
+        .package(url: "https://github.com/realm/SwiftLint", revision: "a876e860ee0e166a05428f430888de5d798c0f8d")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
